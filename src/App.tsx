0fef9a81fd0bcc696fc330a00e179fa3860f77e4
Nabi Isakhanov <nabi.isakhanov@wwecorp.com>
2023-09-27 19:55:35 +0300

    Add todo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,15 @@ export function App() {
   return (
     <>
       <BoardComponent></BoardComponent>
+      <h1 className="font-bold text-xl">TO DO:</h1>
+      <ul>
+        <li>Castle</li>
+        <li>Promotion</li>
+        <li>Checkmate detection</li>
+        <li>Game restart</li>
+        <li>Timers</li>
+        <li>Track of moves (*with import)</li>
+      </ul>
     </>
   )
 }
